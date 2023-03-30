2435a1205302f3d86d891c8f33800c0442adff47
Enital <117684114+Enital@users.noreply.github.com>
2023-03-30 15:04:56 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -27,26 +27,3 @@ let lightbox = new SimpleLightbox(".gallery .gallery__link", {
     close: false,
     enableKeyboard: true,
 });
-
-//=========================================================================================================================================
-// const galleryEl = document.querySelector('.gallery');
-// function createGalleryItem(items) {
-//   return items
-//     .map(({ preview, original, description }) => {
-//       return `<li class="gallery__item">
-//         <a class="gallery__link" href="${preview}"><img class="gallery__image" src="${original}" alt="${description}" title=""/></a>
-//         </li>`;
-//     })
-//     .join('');
-// }
-// const addGalleryItem = createGalleryItem(galleryItems);
-// galleryEl.insertAdjacentHTML('beforeend', addGalleryItem);
-
-// //=========================================================================================================================================
-// var lightbox = new SimpleLightbox('.gallery .gallery__link', {
-//   captionPosition: 'bottom',
-//   captionsData: 'alt',
-//   captionDelay: 250,
-//   close: false,
-//   enableKeyboard: true,
-// });
