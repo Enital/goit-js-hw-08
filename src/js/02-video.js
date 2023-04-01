ba727f13121d14f9d6531b0a031bb700cbe38221
Enital <117684114+Enital@users.noreply.github.com>
2023-04-01 11:25:01 +0300

    01.04.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,53 +1,17 @@
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-// const data = function (data) {
-//     localStorage.setItem('videoplayer-current-time', data.seconds);
-// };
-
-// player.on('timeupdate', throttle(data, 1000));
-
-// const timeBeforeFailure = Number(
-//     localStorage.getItem('videoplayer-current-time')
-// );
-
-
-// player.setCurrentTime(timeBeforeFailure).catch(function (error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//         console.log('range error!');
-//         break;
-
-//         default:
-//         console.log('error!');
-//         break;
-//     }
-// });
-
+const iframeEl=document.querySelector("#vimeo-player")
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+const player = new Player(iframeEl);
 
-const iframeEl=document.querySelector("#vimeo-player")
-// let throttle = require('lodash.throttle')
-
-const player = new Player(iframeEl,
-    {
-    id: 19231868,
-    width: 640
-    }
-);
-
-player.on('timeupdate', throttle(function(data) {
-    let timeOnPlayer = data.seconds;
+function saveTime() {
+    let playTime = data.seconds;
     try {
-        localStorage.setItem("videoplayer-current-time", timeOnPlayer);
+        localStorage.setItem("videoplayer-current-time", playTime);
     } catch (error) {
-        console.log("Sorry, we have some problem with player:", error.name);
+        console.log("Sorry, but some problem with player:", error.name);
     }
-}, 1000));
+}
+player.on('timeupdate', throttle(saveTime, 1000));
 
 
 if (localStorage.getItem("videoplayer-current-time")) { 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
+const formEl = document.querySelector(".feedback-form");
+const inputEmailEl = formEl.elements.email;
+const textareaMessageEl = formEl.elements.message;
+let formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+
+addValueFromLocalStorage()
+
+function addValueFromLocalStorage() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const savedMessageFromJSON = JSON.parse(savedMessage);
+    if (savedMessage) { 
+        const { email = "", message = "" } = savedMessageFromJSON;
+        inputEmailEl.value = email;
+        textareaMessageEl.value = message;
+    } 
+    
+}
+
+
+formEl.addEventListener("input", throttle(formInput, 500));
+
+function formInput(event) {
+    formData.email = inputEmailEl.value;
+    formData.message = textareaMessageEl.value;
+
+    const formDataJson=JSON.stringify(formData)
+    try {
+        localStorage.setItem(STORAGE_KEY, formDataJson)
+    } catch(error) {
+        console.log("We have some problems with loading. Issue:", error)
+    }
+}
+
+formEl.addEventListener("submit", submitFormBtn)
+
+function submitFormBtn(event) {
+    event.preventDefault();
+    if (!formData.hasOwnProperty("email") && !formData.hasOwnProperty("message")) {
+        return
+    }
+    console.log(formData);
+    formData = {};
+    localStorage.removeItem(STORAGE_KEY)
+    event.currentTarget.reset()
+}
 
